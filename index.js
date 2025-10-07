6a004daddbe282055900da131e09888bf280fc62
amosTimi <32045442+amosTimi@users.noreply.github.com>
2025-10-07 16:41:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -9,3 +9,16 @@
 'use strict';
 
 module.exports = require('./lib/express');
+
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello from Azure Express App!");
+});
+
+const port = process.env.PORT || 3000;  // 👈 important!
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
+});
+
